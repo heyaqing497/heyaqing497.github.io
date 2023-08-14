b45007684ffe8bb88d76e668c5488cebeb92fba7
heyaqing <1159056851@qq.com>
2023-08-14 18:01:42 +0800

    修改配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,28 @@
 import { defineConfig } from "umi";
 
+//github仓库名称
+const defaultPath = "/sum-up";
+//打包后gh-pages默认会拼接仓库名称在路径上
+const baseUrl = process.env.NODE_ENV === "production" ? defaultPath : "";
+
+const logo = `${baseUrl}/images/logo.png`;
+
 export default defineConfig({
-  routes: [
-    { path: "/", component: "index" },
-    { path: "/docs", component: "docs" },
+  base: defaultPath,
+  publicPath: `${baseUrl}/`,
+  title: "标题",
+  favicon: logo,
+  logo: logo,
+  outputPath: "docs-dist",
+  mode: "site",
+  extraBabelPlugins: [
+    [
+      "babel-plugin-import",
+      {
+        libraryName: "antd", //配置antd全局样式
+        libraryDirectory: "es",
+        style: true,
+      },
+    ],
   ],
-  npmClient: 'pnpm',
 });
