589943d6ada24b8fb963e880fce4cc064b0b2577
heyaqing <1159056851@qq.com>
2024-03-21 12:16:17 +0800

    build: yml修改

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,14 +5,10 @@ const defaultPath = '/github-actions-demo';
 //打包后gh-pages默认会拼接仓库名称在路径上
 const baseUrl = defaultPath;
 
-const logo = `${baseUrl}/images/logo.png`;
-
 export default defineConfig({
   base: defaultPath,
   publicPath: `${baseUrl}/`,
   title: '标题',
-  favicon: logo,
-  logo: logo,
   outputPath: 'docs-dist',
   mode: 'site',
   extraBabelPlugins: [
