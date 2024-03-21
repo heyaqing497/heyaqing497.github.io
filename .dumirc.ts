6d595cbccd88685382b08c64f1815c242746b05a
heyaqing <1159056851@qq.com>
2024-03-21 11:32:09 +0800

    build: yml修改

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from 'dumi';
 
+//github仓库名称
+const defaultPath = '/github-actions-demo';
+//打包后gh-pages默认会拼接仓库名称在路径上
+const baseUrl = defaultPath;
+
+const logo = `${baseUrl}/images/logo.png`;
+
 export default defineConfig({
+  base: defaultPath,
+  publicPath: `${baseUrl}/`,
+  title: '标题',
+  favicon: logo,
+  logo: logo,
   outputPath: 'docs-dist',
-  themeConfig: {
-    name: 'heyaqing',
-  },
-  publicPath: '/github-actions-demo/',
-  base: '/github-actions-demo/',
+  mode: 'site',
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd', //配置antd全局样式
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
 });
